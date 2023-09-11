427e63e678643723134a18da6c09e8f2885e0a7d
ori roll <orir@biot-med.com>
2023-09-11 20:10:27 +0300

    updates by other pr

--- src/utils/authenticate.js
+++ src/utils/authenticate.js
@@ -1,29 +1,61 @@
 import axios from "axios";
 import JWT from "jsonwebtoken";
 import {
-  BIOT_PUBLIC_KEY,
   JWT_ERROR,
   BIOT_JWT_PERMISSION,
   BIOT_BASE_URL,
   BIOT_SERVICE_USER_ID,
   BIOT_SERVICE_USER_SECRET_KEY,
+  GET_PUBLIC_KEY_API_URL,
 } from "../constants.js";
+
+let publicKey = null;
+
+// This prepares the publicKey to be used with jsonwebtoken's parse (in checkJWT function)
+const constructPublicKey = (publicKey) => {
+  return [
+    "-----BEGIN PUBLIC KEY-----",
+    publicKey,
+    "-----END PUBLIC KEY-----",
+  ].join("\n");
+}
+export const getPublicKey = async () => {
+  
+  const response = await axios.get(
+    `${BIOT_BASE_URL}${GET_PUBLIC_KEY_API_URL}`
+  );
+  
+  const { publicKey } = response?.data;
+  return publicKey;
+}
+
+export const checkJWT = async (token, requiredPermission) => {
+  
+  if(!publicKey) {
+    const responsePublicKey = await getPublicKey();
+    publicKey = constructPublicKey(responsePublicKey);
+  }
+  // This validates the token sent by the notification service
+  const jwtData = await JWT.verify(token, publicKey, {
+    algorithms: ["RS512"],
+  });
+  
+  if (!requiredPermission) return;
+  // TODO: If you need to, update this function to add other permissions to be checked in the JWT
+      
+  // Checks the required permission in the token
+  if (!jwtData.scopes?.includes(requiredPermission)) {
+    throw new Error(
+      `JWT does not have the required permissions. Missing: ${requiredPermission}`
+    );
+  }
+  return;
+}
 
 export const authenticate = async (token, traceId) => {
   try {
-    // This validates the token sent by the notification service
-    const jwtData = await JWT.verify(token, BIOT_PUBLIC_KEY, {
-      algorithms: ["RS512"],
-    });
-    // This checks the token's permission
-
-    if (BIOT_JWT_PERMISSION) {
-      if (!jwtData.scopes?.includes(BIOT_JWT_PERMISSION)) {
-        throw new Error(
-          `JWT does not have the required permissions. Missing: ${BIOT_JWT_PERMISSION}`
-        );
-      }
-    }
+    await checkJWT(token, BIOT_JWT_PERMISSION);
+    return;
   } catch (error) {
     throw new Error(JWT_ERROR, { cause: error });
   }
